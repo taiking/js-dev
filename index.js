d9200af9a9783977ec8bd60591ff1a2dc54ccb90
taiking <c.tsujiyan727@gmail.com>
2020-12-13 15:33:28 +0900

    complete

--- index.js
+++ index.js
@@ -5,20 +5,30 @@ fetch("http://localhost:8081/tweets")
       const div = document.createElement("div")
       const span = document.createElement("span");
       span.innerText = data[i].tweet;
+      const deleteButton = document.createElement("button")
+      deleteButton.innerText = "削除"
+      deleteButton.onclick = function() {
+        fetch("http://localhost:8081/tweets/" + data[i].id, {
+          method: "DELETE"
+        }).then((_) => {
+          div.remove()
+        })
+      }
       const like = document.createElement("button")
       like.innerText = "いいね"
       const likeCount = document.createElement("span")
-      likeCount.innerText = data[i].likes
+      likeCount.innerText = data[i].likes 
       like.onclick = function() {
-      fetch("http://localhost:8081/tweets/like", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ id: data[i].id }),
-      }).then((_) => {
-        likeCount.innerText = parseInt(likeCount.innerText) + 1
-      })
+        fetch("http://localhost:8081/tweets/like", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ id: data[i].id }),
+        }).then((_) => {
+          likeCount.innerText = parseInt(likeCount.innerText) + 1
+        })
       }
       div.appendChild(span)
+      div.appendChild(deleteButton)
       div.appendChild(like)
       div.appendChild(likeCount)
       document.getElementsByClassName("tweets")[0].appendChild(div);
@@ -26,15 +36,19 @@ fetch("http://localhost:8081/tweets")
   });
 
 function onTweet() {
-  const tweet = document.getElementsByClassName("tweet")[0].value;
-  const username = document.getElementsByClassName("username")[0].value;
-  fetch("http://localhost:8081/tweets", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ tweet: tweet, user: username }),
+  const tweet = document.getElementsByClassName("tweet")[0].value
+  const username = document.getElementsByClassName("username")[0].value
+  if (tweet.length === 0 || username === 0) {
+    alert('ツイート内容とユーザー名は1文字以上入力してください。')
+    return
+  }
+  fetch("http://localhost:8081/tweets", { 
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ tweet: tweet, user: username }),
   })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    });
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+  });
 }
